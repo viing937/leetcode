390ddb237ebf0506248a7571019105a70a32a324
ving <viing937@gmail.com>
2015-06-16 11:13:15 +0800

    Tue Jun 16 11:13:15 CST 2015

---
+++ algorithms/91.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numDecodings(string s)
+    {
+        if ( s == "" || s[0] == '0' )
+            return 0;
+        vector<int> dp;
+        dp.push_back(1);
+        dp.push_back(1);
+        for ( unsigned int i = 1; i < s.size(); i++ )
+        {
+            int tmp = 0;
+            if ( s[i] != '0' )
+                tmp += dp[i];
+            if ( s[i-1] == '1' || ( s[i-1]=='2' && s[i] >= '0' && s[i] <= '6' ) )
+                tmp += dp[i-1];
+            dp.push_back(tmp);
+        }
+        return dp.back();
+    }
+};
+
+int main()
+{
+    Solution solution;
+    cout << solution.numDecodings("10") << endl;
+    return 0;
+}
