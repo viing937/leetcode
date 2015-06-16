a4ab4f4a88628b7def3947675ddb63691d0a5c97
ving <viing937@gmail.com>
2015-06-16 17:43:46 +0800

    Tue Jun 16 17:43:46 CST 2015

---
+++ algorithms/198.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums)
+    {
+        if ( nums.empty() )
+            return 0;
+        if ( nums.size() < 2 )
+            return nums[0];
+        int p = 0, q = nums[0], r = nums[1], t;
+        for ( unsigned int i = 2; i < nums.size(); i++ )
+        {
+            t = (p>q?p:q)+nums[i];
+            p = q;
+            q = r;
+            r = t;
+        }
+        return q>r ? q : r;
+    }
+};
+
+int main()
+{
+    vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(1);
+    nums.push_back(1);
+    nums.push_back(2);
+    Solution solution;
+    cout << solution.rob(nums) << endl;
+    return 0;
+}
