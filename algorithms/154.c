5d638793b7b3873a06c731b154e49c45b75e9e01
ving <viing937@gmail.com>
2015-06-25 17:01:37 +0800

    Thu Jun 25 17:01:37 CST 2015

--- algorithms/154.c
+++ algorithms/154.c
@@ -2,28 +2,35 @@
 #include <stdlib.h>
 #include <memory.h>
 
+int min(int a, int b)
+{
+    return a<b ? a : b;
+}
+
 int binary(int l, int r, int arr[])
 {
-    if ( l == r || arr[l] < arr[r] )
-        return l;
+    if ( l >= r )
+        return arr[l];
+    if ( r-l == 1 && arr[l]==arr[r] )
+        return arr[l];
     int m = (l+r)>>1;
-    if ( arr[m]<arr[l] && arr[m]<=arr[r] )
-        return binary(l, m, arr);
-    else
+    if ( arr[m] == arr[r] )
+    {
+        if ( arr[m] != arr[l] )
+            return binary(l, m, arr);
+        return min(binary(m+1, r, arr), binary(l+1, m-1, arr));
+    }
+    else if ( arr[m] > arr[r] )
         return binary(m+1, r, arr);
+    else
+        return binary(l, m, arr);
 }
 
 int findMin(int* nums, int numsSize)
 {
-    int *newnums = (int *)malloc(sizeof(int)*numsSize);
-    memcpy(newnums, nums, sizeof(int)*numsSize);
-    int index = 1;
-    for ( int i = 1; i < numsSize; i++ )
-        if ( newnums[index-1] != newnums[i] )
-            newnums[index++] = newnums[i];
-    int result = newnums[binary(0, index-1, newnums)];
-    free(newnums);
-    return result;
+    if ( numsSize == 0 )
+        return 0;
+    return binary(0, numsSize-1, nums);
 }
 
 int main()
