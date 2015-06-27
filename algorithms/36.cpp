c3d2a05229200a63d0dfdb975ffe65944bb7c4b0
ving <viing937@gmail.com>
2015-06-27 15:06:07 +0800

    Sat Jun 27 15:06:07 CST 2015

--- algorithms/36.cpp
+++ algorithms/36.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstring>
 #include <vector>
 using namespace std;
 
@@ -8,52 +7,20 @@ class Solution
 public:
     bool isValidSudoku(vector< vector<char> >& board)
     {
-        int arr[10];
-        for ( unsigned int i = 0; i < board.size(); i++ )
+        int row[9][9] = {0};
+        int col[9][9] = {0};
+        int block[3][3][9] = {0};
+        for ( int i = 0; i < 9; i++ )
         {
-            memset(arr, 0, sizeof(arr));
-            for ( unsigned int j = 0; j < board[i].size(); j++ )
+            for ( int j = 0; j < 9; j++ )
             {
-                if ( board[i][j] >= '0' && board[i][j] <= '9' )
+                if ( board[i][j] != '.')
                 {
-                    if ( arr[board[i][j]-'0'] )
+                    if ( row[i][board[i][j]-'1'] || col[j][board[i][j]-'1'] || block[i/3][j/3][board[i][j]-'1'] )
                         return false;
-                    else
-                        arr[board[i][j]-'0'] = 1;
-                }
-            }
-        }
-        for ( unsigned int i = 0; i < board[0].size(); i++ )
-        {
-            memset(arr, 0, sizeof(arr));
-            for ( unsigned int j = 0; j < board.size(); j++ )
-            {
-                if ( board[j][i] >= '0' && board[j][i] <= '9' )
-                {
-                    if ( arr[board[j][i]-'0'] )
-                        return false;
-                    else
-                        arr[board[j][i]-'0'] = 1;
-                }
-            }
-        }
-        for ( unsigned int i = 0; i < board.size(); i += 3 )
-        {
-            for ( unsigned int j = 0; j < board[i].size(); j += 3 )
-            {
-                memset(arr, 0, sizeof(arr));
-                for ( int m = 0; m < 3; m++ )
-                {
-                    for ( int n = 0; n < 3; n++ )
-                    {
-                        if ( board[i+m][j+n] >= '0' && board[i+m][j+n] <= '9' )
-                        {
-                            if ( arr[board[i+m][j+n]-'0'] )
-                                return false;
-                            else
-                                arr[board[i+m][j+n]-'0'] = 1;
-                        }
-                    }
+                    row[i][board[i][j]-'1'] = 1;
+                    col[j][board[i][j]-'1'] = 1;
+                    block[i/3][j/3][board[i][j]-'1'] = 1;
                 }
             }
         }
