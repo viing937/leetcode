a9edf14f6e36a94ed3c783dd11e895b0c9c1831f
ving <viing937@gmail.com>
2015-06-28 11:21:48 +0800

    Sun Jun 28 11:21:48 CST 2015

---
+++ algorithms/112.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    bool hasPathSum(TreeNode* root, int sum)
+    {
+        if ( !root )
+            return false;
+        if ( root->val == sum && !root->left && !root->right )
+            return true;
+        return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
+    }
+};
+
+int main()
+{
+    return 0;
+}
