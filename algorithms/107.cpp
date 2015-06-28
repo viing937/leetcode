49586cd9b943d27ee0176e68883bd228a7ee5153
ving <viing937@gmail.com>
2015-06-28 10:56:26 +0800

    Sun Jun 28 10:56:26 CST 2015

---
+++ algorithms/107.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+private:
+    void go(vector< vector<int> > &result, struct TreeNode *root, int depth)
+    {
+        if ( !root )
+            return;
+        if ( depth >= (int)result.size() )
+        {
+            vector<int> level;
+            level.push_back(root->val);
+            result.push_back(level);
+        }
+        else
+        {
+            result[depth].push_back(root->val);
+        }
+        go(result, root->left, depth+1);
+        go(result, root->right, depth+1);
+    }
+public:
+    vector< vector<int> > levelOrderBottom(TreeNode* root)
+    {
+        vector< vector<int> > result;
+        go(result, root, 0);
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
+int main()
+{
+    return 0;
+}
