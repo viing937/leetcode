bb0281b30207526b5725b03b309747d57efcd71d
ving <viing937@gmail.com>
2015-07-13 18:38:49 +0800

    Mon Jul 13 18:38:48 CST 2015

---
+++ algorithms/46.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+    void dfs(vector<int> &nums, vector< vector<int> > &result, vector<int> &per, vector<bool> &visited)
+    {
+        if ( per.size() == nums.size() )
+        {
+            result.push_back(per);
+            return;
+        }
+        else
+        {
+            for ( int i = 0; i < (int)nums.size(); i++ )
+            {
+                if ( !visited[i] )
+                {
+                    visited[i] = true;
+                    per.push_back(nums[i]);
+                    dfs(nums, result, per, visited);
+                    per.pop_back();
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+public:
+    vector< vector<int> > permute(vector<int>& nums)
+    {
+        vector< vector<int> > result;
+        vector<int> per;
+        vector<bool> visited(nums.size(), false);
+        dfs(nums, result, per, visited);
+        return result;
+    }
+};
+
+int main()
+{
+    return 0;
+}
