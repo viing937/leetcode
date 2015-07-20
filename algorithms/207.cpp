8b14293a5d6c34ef6ff984562b9107314e500564
ving <viing937@gmail.com>
2015-07-20 23:53:32 +0800

    Mon Jul 20 23:53:32 CST 2015

--- algorithms/207.cpp
+++ algorithms/207.cpp
@@ -5,26 +5,26 @@ using namespace std;
 class Solution
 {
 private:
-    bool dfs(vector< vector<int> > &graph, vector<bool> &visited, int node)
+    bool dfs(vector< vector<int> > &graph, vector<int> &visited, int node)
     {
-        if ( visited[node] )
+        if ( visited[node] == 1 )
             return true;
-        visited[node] = true;
+        visited[node] = 1;
         for ( auto it = graph[node].begin(); it != graph[node].end(); it++ )
             if ( dfs(graph, visited, *it) )
                 return true;
-        visited[node] = false;
+        visited[node] = 2;
         return false;
     }
 public:
     bool canFinish(int numCourses, vector< pair<int, int> >& prerequisites)
     {
         vector< vector<int> > graph(numCourses);
-        vector<bool> visited(numCourses, false);
+        vector<int> visited(numCourses, 0);
         for ( auto it = prerequisites.begin(); it != prerequisites.end(); it++ )
             graph[it->first].push_back(it->second);
         for ( int i = 0; i < numCourses; i++ )
-            if ( dfs(graph, visited, i) )
+            if ( visited[i] == 0 && dfs(graph, visited, i) )
                 return false;
         return true;
     }
