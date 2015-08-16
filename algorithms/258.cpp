282f102400350e1d16308f696b3a6a26be2658e7
ving <viing937@gmail.com>
2015-08-16 21:39:20 +0800

    Sun Aug 16 21:39:20 CST 2015

--- algorithms/258.cpp
+++ algorithms/258.cpp
@@ -6,17 +6,7 @@ class Solution
 public:
     int addDigits(int num)
     {
-        while ( num > 9 )
-        {
-            int newnum = 0;
-            while ( num )
-            {
-                newnum += num%10;
-                num /= 10;
-            }
-            num = newnum;
-        }
-        return num;
+        return (num-1)%9+1;
     }
 };
 
