46002c7926e09f8df1f9a8011c72fd1852f7fe83
ving <viing937@gmail.com>
2015-08-20 20:09:07 +0800

    Thu Aug 20 20:09:07 CST 2015

---
+++ algorithms/263.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isUgly(int num)
+    {
+        if ( num == 0 )
+            return false;
+        while ( num%2 == 0 )
+            num /= 2;
+        while ( num%3 == 0 )
+            num /= 3;
+        while ( num%5 == 0 )
+            num /= 5;
+        if ( num == 1 )
+            return true;
+        else
+            return false;
+    }
+};
+
+int main()
+{
+    return 0;
+}
