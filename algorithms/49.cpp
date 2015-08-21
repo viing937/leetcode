9bab19bc1a34df9fdb1183accfe56c365fac0c40
ving <viing937@gmail.com>
2015-08-21 23:47:39 +0800

    Fri Aug 21 23:47:39 CST 2015

--- algorithms/49.cpp
+++ algorithms/49.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <algorithm>
 using namespace std;
 
 class Solution
 {
-private:
-    string hashstring(string s)
-    {
-        sort(s.begin(), s.end());
-        return s;
-    }
 public:
     vector< vector<string> > groupAnagrams(vector<string>& strs)
     {
         sort(strs.begin(), strs.end());
-        map< string, vector<int> > mp;
-        for ( unsigned int i = 0; i < strs.size(); ++i )
-            mp[hashstring(strs[i])].push_back(i);
         vector< vector<string> > result;
-        for ( auto it = mp.begin(); it != mp.end(); ++it )
+        unordered_map< string, int > mp;
+        for ( unsigned int i = 0; i < strs.size(); ++i )
         {
-            result.push_back(vector<string>());
-            for ( unsigned int i = 0; i < (it->second).size(); ++i )
-                result.back().push_back(strs[(it->second)[i]]);
+            string hash = strs[i];
+            sort(hash.begin(), hash.end());
+            auto it = mp.find(hash);
+            if ( it != mp.end() )
+                result[it->second].push_back(strs[i]);
+            else
+            {
+                mp[hash] = result.size();
+                result.push_back(vector<string>(1, strs[i]));
+            }
         }
         return result;
     }
