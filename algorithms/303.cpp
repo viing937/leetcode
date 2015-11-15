759af0e410b3ae7a8a42dc625318a227eef32c6a
ving <viing937@gmail.com>
2015-11-15 09:47:49 +0800

    Range Sum Query - Immutable

--- algorithms/303.cpp
+++ algorithms/303.cpp
@@ -9,16 +9,19 @@ private:
 public:
     NumArray(vector<int> &nums)
     {
-        partial_sum.push_back(0);
         if ( nums.size() == 0 )
             return;
+        int n = nums.size();
         partial_sum.push_back(nums[0]);
-        for ( unsigned int i = 1; i < nums.size(); ++i )
+        for ( int i = 1; i < n; ++i )
             partial_sum.push_back(partial_sum.back()+nums[i]);
     }
     int sumRange(int i, int j)
     {
-        return partial_sum[j+1]-partial_sum[i];
+        if ( i == 0 )
+            return partial_sum[j];
+        else
+            return partial_sum[j]-partial_sum[i-1];
     }
 };
 
