60199306d49aabc4b700c897e3387cb10cf53f2b
ving <viing937@gmail.com>
2017-04-20 11:23:46 +0800

    Recover Binary Search Tree

--- algorithms/99.cpp
+++ algorithms/99.cpp
@@ -13,27 +13,40 @@ class Solution {
 private:
     TreeNode *firstElement, *secondElement;
     TreeNode *prevElement;
-    void traverse(TreeNode *root) {
-        if (!root)
-            return;
 
-        traverse(root->left);
-
-        if (!firstElement && prevElement && prevElement->val >= root->val)
+    void doSomething(TreeNode *node) {
+        if (!firstElement && prevElement && prevElement->val >= node->val)
             firstElement = prevElement;
 
-        if (firstElement && prevElement->val >= root->val)
-            secondElement = root;
+        if (firstElement && prevElement->val >= node->val)
+            secondElement = node;
 
-        prevElement = root;
-
-        traverse(root->right);
+        prevElement = node;
     }
 public:
     void recoverTree(TreeNode *root) {
         firstElement = secondElement = prevElement = NULL;
 
-        traverse(root);
+        TreeNode *cur = root, *prev = NULL;
+        while (cur) {
+            if (!cur->left) {
+                doSomething(cur);
+                cur = cur->right;
+            } else {
+                prev = cur->left;
+                while (prev->right && prev->right != cur)
+                    prev = prev->right;
+
+                if (prev->right == cur) {
+                    prev->right = NULL;
+                    doSomething(cur);
+                    cur = cur->right;
+                } else {
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+            }
+        }
 
         swap(firstElement->val, secondElement->val);
         return;
