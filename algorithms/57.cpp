b1543856bf5660b5b07114202482ef39a85444b1
ving <viing937@gmail.com>
2017-05-28 12:54:12 +0800

    Insert Interval

--- algorithms/57.cpp
+++ algorithms/57.cpp
@@ -12,19 +12,17 @@ struct Interval {
 
 class Solution {
 private:
-    static bool compare(const Interval &a, const Interval &b) {
-        if (a.start == b.start)
+    static bool compare (const Interval &a, const Interval &b) {
+        if (a.start == b. start)
             return a.end < b.end;
         else
             return a.start < b.start;
     }
 public:
-    vector<Interval> merge(vector<Interval> &intervals) {
-        vector<Interval> result;
-        if (intervals.size() == 0)
-            return result;
+    vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
+        intervals.push_back(newInterval);
         sort(intervals.begin(), intervals.end(), compare);
-        result.push_back(intervals[0]);
+        vector<Interval> result(1, intervals[0]);
         for (int i = 1; i < intervals.size(); i++) {
             if (intervals[i].start <= result.back().end)
                 result.back().end = max(result.back().end, intervals[i].end);
