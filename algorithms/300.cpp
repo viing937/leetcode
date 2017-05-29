86a8fa68d625d25a8eafca999c0371a1f3b918b2
ving <viing937@gmail.com>
2017-05-29 18:47:16 +0800

    Longest Increasing Subsequence

--- algorithms/300.cpp
+++ algorithms/300.cpp
@@ -3,29 +3,24 @@
 #include <algorithm>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums)
-    {
+    int lengthOfLIS(vector<int> &nums) {
         int n = nums.size();
-        if ( n == 0 )
+        if (n == 0)
             return 0;
-
         vector<int> tails;
-        for ( int i = 0; i < n; ++i )
-        {
+        for (int i = 0; i < n; ++i) {
             auto it = lower_bound(tails.begin(), tails.end(), nums[i]);
-            if ( it == tails.end() )
+            if (it == tails.end())
                 tails.push_back(nums[i]);
-            else if ( *it > nums[i] )
+            else if (*it > nums[i])
                 *it = nums[i];
         }
         return tails.size();
     }
 };
 
-int main()
-{
+int main() {
     return 0;
 }
