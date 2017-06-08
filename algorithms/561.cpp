5f68e98464bb57e56b9081aafa50c1da3b1c1922
ving <viing937@gmail.com>
2017-06-08 17:15:51 +0800

    Array Partition I

---
+++ algorithms/561.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int arrayPairSum(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        int s = 0;
+        for (int i = 0; i < nums.size(); i+=2)
+            s += nums[i];
+        return s;
+    }
+};
+
+int main() {
+    return 0;
+}
