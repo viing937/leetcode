1b17482e4001397eae01f3fb60d74b5c23b24be4
Ving <viing937@gmail.com>
2017-06-20 17:54:55 +0800

    Find All Anagrams in a String

--- algorithms/438.cpp
+++ algorithms/438.cpp
@@ -3,28 +3,22 @@
 using namespace std;
 
 class Solution {
-private:
-    bool isAnagram(const vector<int> &cnt, const vector<int> &tcnt) {
-        for (int i = 0; i < 26; i++)
-            if (cnt[i] != tcnt[i])
-                return false;
-        return true;
-    }
 public:
     vector<int> findAnagrams(string s, string p) {
         vector<int> result;
         if (s.size() < p.size())
             return result;
-        vector<int> cnt(26, 0), tcnt(26, 0);
+        vector<int> hash(128, 0);
         for (auto ch: p)
-            cnt[ch-'a'] += 1;
-        for (int i = 0; i < s.size(); i++) {
-            tcnt[s[i]-'a'] += 1;
-            if (i >= p.size())
-                tcnt[s[i-p.size()]-'a'] -= 1;
-            if (i+1 >= p.size())
-                if (isAnagram(cnt, tcnt))
-                    result.push_back(i-p.size()+1);
+            hash[ch] += 1;
+        int left = 0, right = 0, count = p.size();
+        while (right < s.size()) {
+            if (hash[s[right++]]-- >= 1)
+                count -= 1;
+            if (count == 0)
+                result.push_back(left);
+            if (right-left == p.size() && hash[s[left++]]++ >= 0)
+                count += 1;
         }
         return result;
     }
