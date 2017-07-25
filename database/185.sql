4578ab12fede77d29cad770649513b45bb16226c
Ving <viing937@gmail.com>
2017-07-25 14:05:01 +0800

    Department Top Three Salaries

---
+++ database/185.sql
@@ -0,0 +1,8 @@
+SELECT d.Name Department, e1.Name Employee, e1.Salary
+FROM Employee e1
+JOIN Department d ON e1.DepartmentId = d.Id
+WHERE 3 > (SELECT COUNT(DISTINCT(e2.Salary)) 
+                  FROM Employee e2 
+                  WHERE e2.Salary > e1.Salary 
+                  AND e1.DepartmentId = e2.DepartmentId
+                  );
