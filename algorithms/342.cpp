994fd4c8aee373a73260ecb44cf10e1f643b9963
Ving <viing937@gmail.com>
2017-08-02 14:07:48 +0800

    Power of Four

---
+++ algorithms/342.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        return num > 0 && (num&(num-1)) == 0 && (num&0x55555555) != 0;
+    }
+};
+
+int main() {
+    return 0;
+}
