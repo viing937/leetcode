4a8440ee104a26c75ebef95c565942f451c057ea
Ving <viing937@gmail.com>
2017-08-16 18:24:03 +0800

    Range Addition II

---
+++ algorithms/598.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>> &ops) {
+        for (int i = 0; i < ops.size(); i++) {
+            m = min(m, ops[i][0]);
+            n = min(n, ops[i][1]);
+        }
+        return m*n;
+    }
+};
+
+int main() {
+    return 0;
+}
