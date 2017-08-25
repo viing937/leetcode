aa662da3a89b4967165c015f227faf6759176396
Ving <viing937@gmail.com>
2017-08-25 16:15:29 +0800

    Number Complement

--- algorithms/476.cpp
+++ algorithms/476.cpp
@@ -4,14 +4,9 @@ using namespace std;
 class Solution {
 public:
     int findComplement(int num) {
-        int mask = num;
-        for (int i = 30; i >= 0; i--) {
-            if (!(mask & (1<<i))) continue;
-            for (int j = 0; j <= i; j++)
-                mask |= 1<<j;
-            break;
-        }
-        return (~num)&mask;
+        int mask = ~0;
+        while (num & mask) mask <<= 1;
+        return (~num) & (~mask);
     }
 };
 
