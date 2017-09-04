52fb7d2a5a40efb9cf92263c1cc8318c94a63287
Ving <viing937@gmail.com>
2017-09-04 09:16:14 +0800

    K Inverse Pairs Array

---
+++ algorithms/629.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+private:
+    const int MOD = 1000000007;
+public:
+    int kInversePairs(int n, int k) {
+        vector<vector<int>> dp(n+1, vector<int>(k+1, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= k; j++) {
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % MOD;
+                if (j-i >= 0) dp[i][j] = (dp[i][j] + MOD - dp[i-1][j-i]) % MOD;
+            }
+        }
+        return dp[n][k];
+    }
+};
+
+int main() {
+    Solution solution;
+    solution.kInversePairs(20, 20);
+    return 0;
+}
