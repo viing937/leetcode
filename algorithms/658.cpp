6c6eacdd6d2b060092565934780eb5a5eea1bd72
Ving <viing937@gmail.com>
2017-09-08 17:54:04 +0800

    Find K Closest Elements

--- algorithms/658.cpp
+++ algorithms/658.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     vector<int> findClosestElements(vector<int> &arr, int k, int x) {
-        auto compare  = [&x](const int &a, const int &b) {
-            return !(abs(a-x) < abs(b-x) || abs(a-x) == abs(b-x) && a < b);
-        };
-        priority_queue<int, vector<int>, decltype(compare)> pq(compare, arr);
-        vector<int> result(k);
-        for (int i = 0; i < k; i++) {
-            result[i] = pq.top();
-            pq.pop();
+        int n = arr.size();
+        if (n == 0 || k == 0) return vector<int>();
+        if (arr[0] > x) return vector<int>(arr.begin(), arr.begin()+k);
+        int i = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+        int j = i+1;
+        if (i != 0 && abs(arr[i-1]-x) < abs(arr[i]-x))
+            i -= 1, j -= 1;
+        while (--k) {
+            if (i == 0) {j++; continue;}
+            i -= 1;
+            if (j < n && abs(arr[i]-x) > abs(arr[j]-x))
+                i += 1, j += 1;
         }
-        sort(result.begin(), result.end());
-        return result;
+        return vector<int>(arr.begin()+i, arr.begin()+j);
     }
 };
 
