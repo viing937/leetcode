067bec31fb610ce37dcba6d603eafce7f7f99e54
Ving <viing937@gmail.com>
2017-09-08 18:08:53 +0800

    Find K Closest Elements

--- algorithms/658.cpp
+++ algorithms/658.cpp
@@ -6,20 +6,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> findClosestElements(vector<int> &arr, int k, int x) {
-        int n = arr.size();
-        if (n == 0 || k == 0) return vector<int>();
-        if (arr[0] > x) return vector<int>(arr.begin(), arr.begin()+k);
-        int i = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
-        int j = i+1;
-        if (i != 0 && abs(arr[i-1]-x) < abs(arr[i]-x))
-            i -= 1, j -= 1;
-        while (--k) {
-            if (i == 0) {j++; continue;}
-            i -= 1;
-            if (j < n && abs(arr[i]-x) > abs(arr[j]-x))
-                i += 1, j += 1;
+        int n = arr.size(), i, j;
+        i = lower_bound(arr.begin(), arr.end(), x) - arr.begin() - 1, j = i+1;
+        while (k--) {
+            if (i < 0 || (j < n && abs(arr[i]-x) > abs(arr[j]-x)))
+                j += 1;
+            else
+                i -= 1;
         }
-        return vector<int>(arr.begin()+i, arr.begin()+j);
+        return vector<int>(arr.begin()+i+1, arr.begin()+j);
     }
 };
 
