9cedcee83568342c89f9e6206ff44b88bd7757c5
Ving <viing937@gmail.com>
2017-09-14 13:24:51 +0800

    Minimum Height Trees

--- algorithms/310.cpp
+++ algorithms/310.cpp
@@ -1,41 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <algorithm>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
-private:
-    int helper(int root, int dir, vector<vector<int>> &g, unordered_map<int, unordered_map<int, int>> &m) {
-        if (m[root].find(dir) != m[root].end()) return m[root][dir];
-        int result = 0;
-        for (auto d: g[dir]) {
-            if (d == root) continue;
-            result = max(result, helper(dir, d, g, m));
-        }
-        m[root][dir] = result + 1;
-        return result + 1;
-    }
 public:
     vector<int> findMinHeightTrees(int n, vector<pair<int, int>> &edges) {
-        vector<vector<int>> g(n);
+        if (n == 1) return vector<int>(1, 0);
+        vector<unordered_set<int>> g(n);
         for (int i = 0; i < edges.size(); i++) {
-            g[edges[i].first].push_back(edges[i].second);
-            g[edges[i].second].push_back(edges[i].first);
+            g[edges[i].first].insert(edges[i].second);
+            g[edges[i].second].insert(edges[i].first);
         }
-        unordered_map<int, unordered_map<int, int>> m;
-        vector<int> heights(n);
-        for (int i = 0; i < n; i++) {
-            heights[i] = 0;
-            for (auto d: g[i])
-                heights[i] = max(heights[i], helper(i, d, g, m));
-        }
-        int minHeight = *min_element(heights.begin(), heights.end());
-        vector<int> result;
+        vector<int> leaves;
         for (int i = 0; i < n; i++)
-            if (heights[i] == minHeight)
-                result.push_back(i);
-        return result;
+            if (g[i].size() == 1)
+                leaves.push_back(i);
+        while (n > 2) {
+            n -= leaves.size();
+            vector<int> newLeaves;
+            for (auto i: leaves) {
+                int j = *g[i].begin();
+                g[j].erase(i);
+                if (g[j].size() == 1)
+                    newLeaves.push_back(j);
+            }
+            leaves = newLeaves;
+        }
+        return leaves;
     }
 };
 
