b472bd497c73adc936cfad81349881345509acad
Ving <viing937@gmail.com>
2018-09-18 15:57:45 +0800

    16. 3Sum Closest

--- algorithms/python3/16.py
+++ algorithms/python3/16.py
@@ -1,5 +1,3 @@
-from bisect import bisect_left
-
 class Solution:
     def threeSumClosest(self, nums, target):
         """
@@ -10,15 +8,14 @@ class Solution:
         nums.sort()
         rls = nums[0] + nums[1] + nums[2]
         for i in range(len(nums)-2):
-            for j in range(i+1, len(nums)-1):
-                t = target - nums[i] - nums[j]
-                idx = bisect_left(nums, t, j+1)
-                if j < idx < len(nums):
-                    s = nums[i] + nums[j] + nums[idx]
-                    if abs(rls - target) > abs(s - target):
-                        rls = s
-                if j < idx-1 < len(nums):
-                    s = nums[i] + nums[j] + nums[idx-1]
-                    if abs(rls - target) > abs(s - target):
-                        rls = s
+            j, k = i+1, len(nums)-1
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s == target:
+                    return s
+                rls = s if abs(s-target) < abs(rls-target) else rls
+                if s < target:
+                    j += 1
+                else:
+                    k -= 1
         return rls
