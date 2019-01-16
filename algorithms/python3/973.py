61f63f7607e981353538d400d5efc27280148b02
Ving <viing937@gmail.com>
2019-01-16 20:06:00 +0800

    973. K Closest Points to Origin

---
+++ algorithms/python3/973.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        points.sort(key = lambda x: x[0]**2+x[1]**2)
+        return points[:K]
