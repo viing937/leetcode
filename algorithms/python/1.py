3ec7279c3b5c0043f87093c725f56b3526847ac8
Ving <viing937@gmail.com>
2019-02-27 09:53:22 +0800

    1. Two Sum

---
+++ algorithms/python/1.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        m = {}
+        for i, n in enumerate(nums):
+            if (target-n) in m:
+                return [m[target-n], i]
+            m[n] = i
