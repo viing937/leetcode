e21ca52c98bddf59e43522c0aace5e8cf84350eb
Ving <viing937@gmail.com>
2019-08-29 23:28:51 +0800

    72. Edit Distance

---
+++ algorithms/python/72.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        dp = [[0] * (n + 1) for i in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][0] = i
+        for j in range(n + 1):
+            dp[0][j] = j
+        for i in range(m):
+            for j in range(n):
+                if word1[i] == word2[j]:
+                    dp[i + 1][j + 1] = dp[i][j]
+                else:
+                    dp[i + 1][j + 1] = min(dp[i][j], dp[i + 1][j], dp[i][j + 1]) + 1
+        return dp[m][n]
