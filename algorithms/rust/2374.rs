c631c6501cb37811701e8ebf36049712dfda4856
Ving <viing937@gmail.com>
2023-07-15 10:49:17 +0800

    2374. Node With Highest Edge Score

---
+++ algorithms/rust/2374.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    #[allow(unused)]
+    pub fn edge_score(edges: Vec<i32>) -> i32 {
+        let n = edges.len();
+        let mut scores = vec![0; n];
+        for (i, edge) in edges.into_iter().enumerate() {
+            scores[edge as usize] += i;
+        }
+        let index = scores
+            .iter()
+            .enumerate()
+            .min_by(|(_, a), (_, b)| b.cmp(a))
+            .map(|(i, _)| i)
+            .unwrap();
+        return index as i32;
+    }
+}
+
+#[allow(dead_code)]
+struct Solution;
+
+fn main() {
+    println!("Hello, world!");
+}
