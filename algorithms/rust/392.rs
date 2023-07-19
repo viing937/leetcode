5b5de4473beaf592235b61b282171be16914e3bd
Ving <viing937@gmail.com>
2023-07-19 08:21:35 +0800

    392. Is Subsequence

---
+++ algorithms/rust/392.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    #[allow(unused)]
+    pub fn is_subsequence(s: String, t: String) -> bool {
+        let mut t = t.chars();
+        for c in s.chars() {
+            loop {
+                match t.next() {
+                    Some(ch) => {
+                        if c == ch {
+                            break;
+                        }
+                    }
+                    _ => return false,
+                }
+            }
+        }
+        return true;
+    }
+}
+
+#[allow(dead_code)]
+struct Solution;
+
+fn main() {
+    println!("Hello, world!");
+}
