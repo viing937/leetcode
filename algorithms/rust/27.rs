bd9676fb67c7d17381f964b69c78a738a100ab2c
Ving <viing937@gmail.com>
2023-07-22 08:15:37 +0800

    27. Remove Element

---
+++ algorithms/rust/27.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    #[allow(unused)]
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let mut idx = 0;
+        for i in 0..nums.len() {
+            if nums[i] != val {
+                nums[idx] = nums[i];
+                idx += 1;
+            }
+        }
+        nums.resize(idx, 0);
+        return idx as i32;
+    }
+}
+
+#[allow(dead_code)]
+struct Solution;
+
+fn main() {
+    println!("Hello, world!");
+}
